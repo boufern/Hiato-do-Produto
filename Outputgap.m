745859ed4e176b2b4a02eb64090bc42156325a35
boufern <137409229+boufern@users.noreply.github.com>
2023-06-22 12:47:41 -0300

    Outputgap.m

--- Outputgap.m
+++ Outputgap.m
@@ -1,105 +1,99 @@
-%% Exercíco programa 3 - Exercicio 1
-% ----------------------------------------------------------------
-% Data: Maio/2022
-% Autor: Bruno Fernandes
-% ----------------------------------------------------------------
+%% Output Gap
+% -------------------------------------------------------------------------------------
+% 
+% Purpose: Bayesian estimation of a State-Space model that is able to
+% estimate the economy's actual business cycle and potential output.
+%
+% Target dynamic model with constant parameters:
+% y(t) = mu(t) + eta(t)
+% mu(t) = delta + mu(t-1) + w(t)                   , w(t)~N(0,Q) iid
+% eta(t) = phi1*eta(t-1) + phi2*eta(t-2) + v(t)    , v(t)~N(0,R) iid
+% 
+% where y(t) -- log of output
+%       mu(t) -- potential outup
+%       eta(t) -- output gap    
+%
+% State-space model with constant parameters used for estimation
+% y(t) = H0 + H1*eta(t) + w(t),  w(t)~N(0,Q) iid
+% eta(t) = F*eta(t-1) + G*v(t) ,  v(t)~N(0,R) iid
+% 
+% Inputs:   F  -- 2 x 2 matrix
+%           G  -- 2 x 1 matrix
+%           Q  -- 1 x 1 covariance matrix
+%           H0 -- 1 x 1 vector of constants
+%           H1 -- 2 x 1 matrix
+%           R  -- 1 x 1 covariance matrix
+%           y  -- T x 1 data matrix
+% Outputs:  mu -- (T-1) x (draws-burnin) matrix
+%           eta -- (T-1) x (draws-burnin) matrix
+%
+% Priors and        phi1 ~ N(0.7,0.1)
+% initial values:   phi2 ~ N(0,0.05)
+%                   delta ~ N(0.005,0.01)
+%                   sigma_v ~ IG(5,0.05)
+%                   eta(0) ~ N([0 0], 0.01*I(2))
 
-%Carregar a série "pib.csv"
+% -------------------------------------------------------------------------------------
+% Date: 22/06/2023
+% Author: Bruno Fernandes
+% If you find any error, please contact bruno.fernandes03@outlook.com
+% -------------------------------------------------------------------------------------
+
+PIB = readmatrix("~/Files/Job/pib.csv");
+[T,~] = size(PIB);
 
 rng(123)
 
-% Construindo o vetor de datas trimestrais de 1999 a 2021
+% Building the vector of dates 
 count = 1;
-for i = 1999:2021
+for i = 1996:floor(1996+T/4)
     for j = 1:3:12
-        data(count) = datetime(i, j, 01);
+        date(count) = datetime(i, j, 01);
         count = count + 1;
     end
 end
 
-data.Format = 'mmm-yyyy';
+formatOut = 'mmm-yyyy';
+datestr(date,formatOut);
 
-% Log do PIB dessazonalizado de jan/1999-out/2021
-PIB = PIB(13:104);
+date = date(1:T);
 
-figure(7)
-plot(data, PIB);
-title('Log do PIB dessazonalizado de jan/1999-out/2021')
+% Setting up the parameter priors
+phi_pr = [0.7; 0]; % mean of phi
+Vp_pr = diag([0.1 0.05]); % variance of phi
 
-%Estabelecendo as priors dos parâmetros
-phi_pr = [0.7; 0]; % média de phi
-Vp_pr = diag([0.1 0.05]); % variância de phi
+delta_pr = 0.005; % mean of delta
+Vd_pr = 0.01; % variance of delta
 
-delta_pr = 0.005; % média de delta
-Vd_pr = 0.01; %variância de delta
+nuv_pr = 10; % nuv_pr/2 =  Inverse Gamma distribution alpha of the variance of V
+nuw_pr = 10; % sv_pr/2 = Inverse Gamma distribution beta of the variância of V
 
-nuv_pr = 10; % nuv_pr/2 = alfa da distribuição inversa gamma da variância de V
-nuw_pr = 10; % sv_pr/2 = beta da distribuição inversa gamma de variância de V
+sv_pr = 0.01*nuv_pr; % nuw_pr/2 = Inverse Gamma distribution alpha of the variance of W
+sw_pr = 0.01*nuw_pr; % sw_pr/2 = Inverse Gamma distribution beta of the variância of W
 
-sv_pr = 0.1; % nuw_pr/2 = alfa da distribuição inversa gamma de variância de W
-sw_pr = 0.1; % sw_pr/2 = beta da distribuição inversa gamma de variância de W
+eta_pr = [0; 0]; % mean eta
+p_pr = 0.01*eye(2); % mean of eta
 
-eta_pr = [0; 0]; % média de eta
-p_pr = 0.01*eye(2); % variância de eta
-
-%% a)
-
-% Plot das priors
-grid_norm = -2:0.01:2;
-grid_IG = 0.001:0.001:0.1;
-
-figure(1)
-subplot(2,4,1)
-    hold on;
-    plot(grid_norm, normpdf(grid_norm, phi_pr(1), sqrt(Vp_pr(1, 1))),'LineWidth', 2)
-    xlabel('Phi1')
-subplot(2,4,2)
-    hold on;
-    plot(grid_norm, normpdf(grid_norm, phi_pr(2), sqrt(Vp_pr(2, 2))), 'LineWidth', 2)
-    xlabel('Phi2')
-subplot(2,4,3)
-    hold on;
-    plot(grid_norm, normpdf(grid_norm, delta_pr, sqrt(Vd_pr)), 'LineWidth', 2)
-    xlabel('Delta')
-subplot(2,4,4)
-    hold on;
-    plot(grid_IG, gampdf(1./grid_IG, nuv_pr/2, 2/sv_pr), 'LineWidth', 2)
-    xlabel('Sigma^2_v')
-subplot(2,4,5)
-    hold on;
-    plot(grid_IG, gampdf(1./grid_IG, nuw_pr/2, 2/sw_pr), 'LineWidth', 2)
-    xlabel('Sigma^2_w')
-subplot(2,4,6)
-    hold on;
-    plot(grid_norm, normpdf(grid_norm, eta_pr(1), sqrt(p_pr(1, 1))), 'LineWidth', 2)
-    xlabel('Eta_0')
-subplot(2,4,7)
-    hold on;
-    plot(grid_norm, normpdf(grid_norm, eta_pr(2), sqrt(p_pr(2, 2))), 'LineWidth', 2)
-    xlabel('Eta_1')
-    
-%% d)
-
+%Setting up the paramerters of the estimation
 draws = 7000;
 burnin = 2000;
-T = size(PIB,1);
 
-% Matrizes de armanezamento
+% Storage matrixes 
 phi = nan(draws, 2); 
-sigw = nan(draws, 1); %variância de W
+sigw = nan(draws, 1); % variance of W
 
 delta = nan(draws, 1);
-sigv = nan(draws, 1); %variância de V
+sigv = nan(draws, 1); % variance of V
 
 eta = nan(T-1, 2, draws);
 
-% Valores iniciais (dados pela média de cada parâmetro)
+% Initial Values (given by the mean of each parameter)
 phi(1,:) = phi_pr';
 sigw(1) = (sw_pr/2)/((nuw_pr/2) - 1);
 delta(1) = delta_pr;
 sigv(1) = (sv_pr/2)/((nuv_pr/2) - 1);
 
-% Matrizes iniciais do espaço estado
+% Initial matrixes of the state-space model
 F = [phi(1, :); 1 0];
 G = [1; 0];
 Q = sigw(1);
@@ -107,29 +101,29 @@ H0 = delta(1);
 H1 = [1 -1];
 R = sigv(1);
 
-% valores posteriors dos hiperparâmetros nuv_ps e nuw_ps
+% The hyperparameters posterior values nuv_ps and nuw_ps
 nuv_ps = nuv_pr + T;
 nuw_ps = nuw_pr + T;
 
-% Tirando a primeira diferença do log do Pib
+% Log of Pib first differences
 
 y = PIB(2:end) - PIB(1:end-1);
 
-% Algorítimo de sorteio da posterior com modelo de espaço estado linear
+% Drawing from the state space posterior
 for i = 2:draws
     eta(:,:, i) = carter_kohn_sampler(F, G, Q, H0, H1, R, y, eta_pr, p_pr);
     
-    %Atualização dos hiperparâmetros para sorteio de novos parâmetros da posterior
+    % Hyperparameters updating in order to draw new parameters from the posterior
     Y = eta(2:end, 1, i);
     X = eta(1:end-1, :, i);
     phi_chap = inv(X'*X)*(X'*Y);
     
-    %Atualização de sigw
+    % Updating sigw
     sw_ps = sw_pr + (Y - X*phi(i-1,:)')'*(Y - X*phi(i-1,:)');
     sigw(i) = 1./gamrnd(nuw_ps/2, 2/sw_ps);
     Q = sigw(i);
     
-    %Atualização de phi
+    % Updating phi
     Vp_ps = inv(inv(Vp_pr) + (1/sigw(i))*(X'*X));
     phi_ps = Vp_ps*(inv(Vp_pr)*phi_pr + (1/sigw(i))*(X'*X)*phi_chap); 
     phi(i,:) = mvnrnd(phi_ps, Vp_ps, 1);
@@ -140,12 +134,12 @@ for i = 2:draws
     X = ones(T-2, 1);
     delta_chap = inv(X'*X)*(X'*Y);
     
-    %Atualização de sigv
+    % Updating sigv
     sv_ps = sv_pr + (Y - X*delta(i-1))'*(Y - X*delta(i-1));
     sigv(i) = 1./gamrnd(nuv_ps/2, 2/sv_ps);
     R = sigv(i);
 
-    %Atualização de delta
+    % Updating delta
     Vd_ps = inv(inv(Vd_pr) + (1/sigv(i))*(X'*X));
     delta_ps = Vd_ps*(inv(Vd_pr)*delta_pr + (1/sigv(i))*(X'*X)*delta_chap); 
     delta(i) = mvnrnd(delta_ps, Vd_ps, 1);
@@ -182,13 +176,12 @@ for i = 1:k
 end
 
 
-% Gráfico autocorrelações
-% Função autocorr proveniente do pacote Econometrics dentro da licença da GV 
+% Autocorrelation Graphic
 n_acf = 50;
 window = 50;
 n_draws = draws - burnin;
 
-figure(3)
+figure(1)
 set(gca, 'Color', 'none');
 for i = 1:k % for each variable
     [acf,lag,bounds] = autocorr(theta(:,i),n_acf); 
@@ -210,30 +203,30 @@ legend(s(3),'Sorteios','Média Móvel (50 sorteios)')
 
 %% e)
 
-% Squeeze de mu e eta
+% Squeeze mu and eta
 mu = squeeze(PIB(2:end) - eta(:, 1,:));
 eta = squeeze(eta(:, 1,:));
 
-% Gráfico produto vs. produto Potencial
-figure(4)
+% Actual output vs. the potencial one graphic
+figure(2)
 set(gca, 'Color', 'w');
 hold on
-plot(data(2:end), PIB(2:end), 'k', 'LineWidth', 2)
-plot(data(2:end), mean(mu, 2), 'b')
-shade_interval(data(2:end), quantile(mu, 0.16, 2), 'b--', data(2:end), quantile(mu, 0.84, 2), '--b', 'FillType', [1 2; 2 1], 'FillAlpha', 0.1)
+plot(date(2:end), PIB(2:end), 'k', 'LineWidth', 2)
+plot(date(2:end), mean(mu, 2), 'b')
+shade_interval(date(2:end), quantile(mu, 0.16, 2), 'b--', date(2:end), quantile(mu, 0.84, 2), '--b', 'FillType', [1 2; 2 1], 'FillAlpha', 0.1)
 legend('Produto','Produto Potencial')
 grid on
 grid minor
 
 %% f)
 
-% Gráfico hiato do produto
-figure(5)
+% Output gap graphic
+figure(3)
 set(gca, 'Color', 'w');
 hold on
-plot(data(2:end), zeros(T-1), 'k-.', 'LineWidth', 1.5)
-plot(data(2:end), mean(eta, 2), 'b-', 'LineWidth', 1.5)
-shade_interval(data(2:end), quantile(eta, 0.16, 2), 'r:', data(2:end), quantile(eta, 0.84, 2), 'r:', 'FillType', [1 2; 2 1], 'FillAlpha', 0.1)
+plot(date(2:end), zeros(T-1), 'k-.', 'LineWidth', 1.5)
+plot(date(2:end), mean(eta, 2), 'b-', 'LineWidth', 1.5)
+shade_interval(date(2:end), quantile(eta, 0.16, 2), 'b:', date(2:end), quantile(eta, 0.84, 2), 'b:', 'FillType', [1 2; 2 1], 'FillAlpha', 0.1)
 grid on
 grid minor
 
